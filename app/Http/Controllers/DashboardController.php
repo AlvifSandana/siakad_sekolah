525e8ab2e9d3623e5c41800556c6b5847de86e38
AlvifSandana <asm240800@gmail.com>
2021-06-17 22:56:29 +0700

    create DashboardController and show count of siswa, guru, kelas, and mapel tables

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Siswa;
+use App\Guru;
+use App\Kelas;
+use App\Mapel;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $jumlah_siswa = count(Siswa::all());
+        $jumlah_guru = count(Guru::all());
+        $jumlah_kelas = count(Kelas::all());
+        $jumlah_mapel = count(Mapel::all());
+
+        return view('dashboard.index', [
+            'n_siswa' => $jumlah_siswa,
+            'n_guru' => $jumlah_guru,
+            'n_kelas' => $jumlah_kelas,
+            'n_mapel' => $jumlah_mapel
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
