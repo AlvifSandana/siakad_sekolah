e10fdb5aa0a053eb120e979900dd8927067a2f09
AlvifSandana <asm240800@gmail.com>
2021-06-17 22:56:46 +0700

    change dashbouard route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dasbor');
-});
+Route::get('/', 'DashboardController@index');
+
 
 Route::get('/jadwalpelajaran', function () {
     return view('jadwal');
