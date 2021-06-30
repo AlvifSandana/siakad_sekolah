5f3d29c742be7203f9225f53b2a5e29d1b25d68a
Alvif Sandana Mahardika <asm240800@gmail.com>
2021-06-30 16:03:00 +0700

    try to use ceksesi middleware

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,6 +12,11 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('ceksesi');
+    }
+
     /**
      * Display a listing of the resource.
      *
