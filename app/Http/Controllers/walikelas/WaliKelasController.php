5c5c5bf38d49e2d53a2a09fb45ef5710f4b826fc
Alvif Sandana Mahardika <asm240800@gmail.com>
2021-07-01 13:27:35 +0700

    create controller for logged wali kelas

---
+++ app/Http/Controllers/walikelas/WaliKelasController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\walikelas;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WaliKelasController extends Controller
+{
+    //
+}
