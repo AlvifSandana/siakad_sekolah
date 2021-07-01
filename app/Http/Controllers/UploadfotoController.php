332f846290293fd275a04d408f3d0b82542ab3f3
Alvif Sandana Mahardika <asm240800@gmail.com>
2021-07-01 14:23:38 +0700

    fix upload foto controller

--- app/Http/Controllers/UploadfotoController.php
+++ app/Http/Controllers/UploadfotoController.php
@@ -13,7 +13,8 @@ class UploadfotoController extends Controller
         $this->middleware('ceksesi');
     }
 
-    public function guru(Request $request){
+    public function guru(Request $request, $id)
+    {
         try {
             $photo_dir = 'profile_img/guru';
             $profile_img = $request->file('profile_img');
@@ -21,21 +22,27 @@ class UploadfotoController extends Controller
                 'profile_img' => 'required',
             ];
             $validator = Validator::make($request->all(), $rules);
+
             if ($validator->fails()) {
                 return back()->withErrors($validator)->withInput($request->all());
             }
+
             $data_update = [
-                'profile_img' => $request->session()->get('nip').'_'.$profile_img->getClientOriginalName()
+                'profile_img' => $profile_img->getClientOriginalName()
             ];
-            DB::table('guru')->where('id_guru', '=', $request->session()->get('id_guru'))->update($data_update);
-            $profile_img->move($photo_dir, $request->session()->get('nip').'_'.$profile_img->getClientOriginalName());
+
+            DB::table('guru')->where('id_guru', '=', $id)->update($data_update);
+
+            $profile_img->move($photo_dir, $profile_img->getClientOriginalName());
+            $request->session()->put('profile_img', $profile_img->getClientOriginalName());
             return back()->with('success', 'Foto berhasil diperbarui.');
         } catch (\Throwable $th) {
             return back()->withErrors($th->getMessage());
         }
     }
 
-    public function siswa(Request $request){
+    public function siswa(Request $request, $id)
+    {
         try {
             $photo_dir = 'profile_img/siswa';
             $profile_img = $request->file('profile_img');
@@ -43,14 +50,17 @@ class UploadfotoController extends Controller
                 'profile_img' => 'required',
             ];
             $validator = Validator::make($request->all(), $rules);
+
             if ($validator->fails()) {
                 return back()->withErrors($validator)->withInput($request->all());
             }
+
             $data_update = [
-                'profile_img' => $request->session()->get('nis').'_'.$profile_img->getClientOriginalName()
+                'profile_img' => $profile_img->getClientOriginalName()
             ];
-            DB::table('siswa')->where('id_siswa', '=', $request->session()->get('id_siswa'))->update($data_update);
-            $profile_img->move($photo_dir, $request->session()->get('nis').'_'.$profile_img->getClientOriginalName());
+
+            DB::table('siswa')->where('id_siswa', '=', $id)->update($data_update);
+            $profile_img->move($photo_dir, $profile_img->getClientOriginalName());
             return back()->with('success', 'Foto berhasil diperbarui.');
         } catch (\Throwable $th) {
             return back()->withErrors($th->getMessage());
