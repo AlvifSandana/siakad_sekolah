b91cd1c25f2ecc1036ae8fb0a483efc2f98cee48
Alvif Sandana Mahardika <asm240800@gmail.com>
2021-07-01 13:29:16 +0700

    create logged guru pages

---
+++ resources/views/halaman/guru/account.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.master')
+@section('title', 'Account')
+
+@section('css')
+@endsection
+
+@section('style')
+@endsection
+
+@section('breadcrumb-title')
+<h3>Account Info</h3>
+@endsection
+
+@section('breadcrumb-items')
+<li class="breadcrumb-item">Guru</li>
+<li class="breadcrumb-item active">Account Info</li>
+@endsection
+
+@section('content')
+<div class="row">
+    @include('layouts.flash')
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                @foreach ($guru as $data_guru)
+                    <div class="row">
+                        <div class="col-md-8">
+                          <div class="form-group">
+                            <label class="bmd-label-floating">Nama Lengkap Guru</label>
+                            <input type="text" class="form-control" value="{{ $data_guru->nama_lengkap_guru}}" name="nama_lengkap_guru" disabled>
+                          </div>
+                          <div class="form-group">
+                            <label class="bmd-label-floating">NIP</label>
+                            <input type="text" class="form-control" value="{{ $data_guru->nip }}" name="nip" disabled>
+                          </div>
+                          <div class="form-group">
+                            <label class="bmd-label-floating">Tempat Lahir</label>
+                            <input type="text" class="form-control" value="{{ $data_guru->kota_lahir_guru }}" name="kota_lahir_guru" disabled>
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <img class="img-thumbnail" src="{{ asset('profile_img/guru/'.$data_guru->profile_img) }}" alt="Foto Guru">
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <label class="">Tanggal Lahir</label>
+                            <input type="date" class="form-control" value="{{ $data_guru->tanggal_lahir_guru }}" name="tanggal_lahir_guru" id="tanggal_lahir" disabled>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label class="bmd-label-floating">Jenis Kelamin</label>
+                            <input type="text" class="form-control" value="{{ $data_guru->jenis_kelamin_guru == 'l'? 'Pria' : 'Wanita'}}" disabled>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label class="bmd-label-floating">Agama</label>
+                            <input type="text" value="{{ $data_guru->agama }}" class="form-control" disabled>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <label class="bmd-label-floating">Alamat Guru</label>
+                            <textarea class="form-control" name="alamat_guru" cols="30" rows="5" disabled>{{ $data_guru->alamat_guru }}</textarea>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row">
+                          <div class="col-md-6">
+                              <div class="form-group">
+                                  <label class="bmd-label-floating">Nomor HP</label>
+                                  <input class="form-control" type="tel" value="{{ $data_guru->no_hp_guru }}" name="no_hp_guru" disabled>
+                              </div>
+                          </div>
+                          <div class="col-md-6">
+                              <div class="form-group">
+                                  <label class="bmd-label-floating">Email Guru</label>
+                                  <input class="form-control" type="email" value="{{ $data_guru->email }}" name="email_guru" disabled>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="clearfix"></div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/halaman/guru/index.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.master')
+@section('title', 'Guru Dashboard')
+
+@section('css')
+@endsection
+
+@section('style')
+@endsection
+
+@section('breadcrumb-title')
+<h3>Dashboard</h3>
+@endsection
+
+@section('breadcrumb-items')
+<li class="breadcrumb-item active">Dashboard</li>
+@endsection
+
+@section('content')
+<div class="row">
+    @include('layouts.flash')
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+        <div class="card-header card-header-warning card-header-icon">
+            <p class="card-category">Jumlah Siswa</p>
+            <h4 class="card-title">{{ $n_siswa }}
+            <small>Orang</small>
+            </h4>
+        </div>
+        
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+        <div class="card-header card-header-success card-header-icon">
+            <p class="card-category">Jumlah Guru</p>
+            <h4 class="card-title">{{ $n_guru }} <small>Orang</small></h4>
+        </div>
+
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+        <div class="card-header card-header-danger card-header-icon">
+            <p class="card-category">Mata Pelajaran</p>
+            <h4 class="card-title text-nowrap">{{ $n_mapel }}<small> Mata Pelajaran</small></h4>
+        </div>
+
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+        <div class="card-header card-header-info card-header-icon">
+            <p class="card-category">Jumlah Kelas</p>
+            <h4 class="card-title">{{ $n_kelas }} <small>Ruang</small></h4>
+        </div>
+        </div>
+    </div>
+</div>
+@endsection
