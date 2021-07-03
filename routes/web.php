ecbc9ee98c3783d3e1bbd25cc4f08dc42f72567a
Alvif Sandana Mahardika <asm240800@gmail.com>
2021-07-03 19:56:24 +0700

    add password change and walikelas account route

--- routes/web.php
+++ routes/web.php
@@ -61,10 +61,16 @@ Route::get('/logout', 'LoginController@logout')->name('logout');
 Route::put('siakad/upload/guru/{id}', 'UploadfotoController@guru')->name('guru.upload_foto');
 Route::put('siakad/upload/siswa/{id}', 'UploadfotoController@siswa')->name('siswa.upload_foto');
 
+// password change route
+Route::put('siakad/guru/pwd/change/{id}', 'PasswordController@updatePasswordGuru')->name('changepwd_guru');
+
 // guru route
 Route::get('siakad/guru/dashboard', 'guru\GuruController@dashboard')->name('dashboard_guru');
 Route::get('siakad/guru/account', 'guru\GuruController@accountdetail')->name('account_guru');
 Route::get('siakad/guru/account/edit', 'guru\GuruController@accountedit')->name('account_guru.edit');
 Route::put('siakad/guru/account/update', 'guru\GuruController@accountupdate')->name('account_guru.update');
 // wali kelas route
-Route::get('siakad/walikelas/dashboard', 'walikelas\WaliKelasController@index')->name('dashboard_walikelas');
+Route::get('siakad/walikelas/dashboard', 'walikelas\WaliKelasController@dashboard')->name('dashboard_walikelas');
+Route::get('siakad/walikelas/account', 'walikelas\WaliKelasController@accountdetail')->name('account_walikelas');
+Route::get('siakad/walikelas/account/edit', 'walikelas\WaliKelasController@accountedit')->name('account_walikelas.edit');
+Route::put('siakad/walikelas/account/update', 'walikelas\WaliKelasController@accountupdate')->name('account_walikelas.update');
