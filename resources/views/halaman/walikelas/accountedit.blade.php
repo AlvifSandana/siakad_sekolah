eb3dde3f32b0b221e581833c2d40c54cb521b77d
Alvif Sandana Mahardika <asm240800@gmail.com>
2021-07-03 19:57:07 +0700

    add account info and edit page for walikelas change walikelas logged in dashboard

---
+++ resources/views/halaman/walikelas/accountedit.blade.php
@@ -0,0 +1,174 @@
+@extends('layouts.master')
+@section('title', 'Edit Account')
+
+@section('css')
+@endsection
+
+@section('style')
+@endsection
+
+@section('breadcrumb-title')
+<h3>Edit Account</h3>
+@endsection
+
+@section('breadcrumb-items')
+<li class="breadcrumb-item"><a href="{{ route('account_guru') }}">Account</a></li>
+<li class="breadcrumb-item active">Edit Account</li>
+@endsection
+
+@section('content')
+    <div class="row">
+        @include('layouts.flash')
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Foto Profil</h5>
+                </div>
+                <div class="card-body">
+                    @foreach ($guru as $data_guru)
+                    <form action="{{ route('guru.upload_foto', $data_guru->id_guru) }}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <div class="row">
+                            <div class="col-md-12">
+                                <img src="{{ asset('profile_img/guru/'.$data_guru->profile_img) }}" alt="foto profil" class="img-thumbnail mb-3" style="max-width: 120px">
+                                <input type="file" name="profile_img" id="" class="form-control col-md-6 mb-3">
+                                <div class="pull-right">
+                                    <button type="submit" class="btn btn-warning">Ganti Foto</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Password</h5>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('changepwd_guru', session()->get('id_guru')) }}" method="post">
+                    @csrf
+                    @method('PUT')
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label>Masukkan Password Baru</label>
+                                <input type="password" name="password" class="form-control mb-2">
+                                <label>Ulangi Password</label>
+                                <input type="password" name="password_confirmation" class="form-control">
+                            </div>
+                            <div class="float-right">
+                                <button type="submit" class="btn btn-warning">Perbarui Password</button>
+                            </div>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Data Profil</h5>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('account_walikelas.update', $data_guru->id_guru) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <div class="row">
+                            <div class="col-md-8">
+                              <div class="form-group">
+                                <label class="bmd-label-floating">Nama Lengkap Guru</label>
+                                <input type="text" class="form-control" value="{{ $data_guru->nama_lengkap_guru}}" name="nama_lengkap_guru">
+                              </div>
+                            </div>
+                            <div class="col-md-4">
+                              <div class="form-group">
+                                <label class="bmd-label-floating">NIP</label>
+                                <input type="text" class="form-control" value="{{ $data_guru->nip }}" name="nip">
+                              </div>
+                            </div>
+                          </div>
+                          <div class="row">
+                            <div class="col-md-6">
+                              <div class="form-group">
+                                <label class="bmd-label-floating">Tempat Lahir</label>
+                                <input type="text" class="form-control" value="{{ $data_guru->kota_lahir_guru }}" name="kota_lahir_guru">
+                              </div>
+                            </div>
+                            <div class="col-md-6">
+                              <div class="form-group">
+                                <label class="">Tanggal Lahir</label>
+                                <input type="date" class="form-control" value="{{ $data_guru->tanggal_lahir_guru }}" name="tanggal_lahir_guru" id="tanggal_lahir">
+                              </div>
+                            </div>
+                          </div>
+                          <div class="row">
+                            <div class="col-md-6">
+                              <div class="form-group">
+                                <label class="bmd-label-floating">Jenis Kelamin</label>
+                                <select class="form-control" name="jenis_kelamin_guru">
+                                  <option value="l" {{ $data_guru->jenis_kelamin_guru == 'l' ? 'selected' : '' }}>Laki-laki</option>
+                                  <option value="p" {{ $data_guru->jenis_kelamin_guru == 'p' ? 'selected' : '' }}>Perempuan</option>
+                                </select>
+                              </div>
+                            </div>
+                            <div class="col-md-6">
+                              <div class="form-group">
+                                <label class="bmd-label-floating">Agama</label>
+                                <select class="form-control" name="agama">
+                                  <option value="islam" {{ $data_guru->agama == 'islam'? 'selected' : '' }}>Islam</option>
+                                  <option value="kristen" {{ $data_guru->agama == 'kristen'? 'selected' : '' }}>Kristen</option>
+                                  <option value="katolik" {{ $data_guru->agama == 'katolik'? 'selected' : '' }}>Katolik</option>
+                                  <option value="hindu" {{ $data_guru->agama == 'hindu'? 'selected' : '' }}>Hindu</option>
+                                  <option value="buddha" {{ $data_guru->agama == 'buddha'? 'selected' : '' }}>Buddha</option>
+                                  <option value="konghucu" {{ $data_guru->agama == 'konghucu'? 'selected' : '' }}>Konghucu</option>
+                                </select>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="row">
+                            <div class="col-md-12">
+                              <div class="form-group">
+                                <label class="bmd-label-floating">Alamat Guru</label>
+                                <textarea class="form-control" name="alamat_guru" cols="30" rows="5">{{ $data_guru->alamat_guru }}</textarea>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="row">
+                              <div class="col-md-6">
+                                  <div class="form-group">
+                                      <label class="bmd-label-floating">Nomor HP</label>
+                                      <input class="form-control" type="tel" value="{{ $data_guru->no_hp_guru }}" name="no_hp_guru">
+                                  </div>
+                              </div>
+                              <div class="col-md-6">
+                                  <div class="form-group">
+                                      <label class="bmd-label-floating">Email Guru</label>
+                                      <input class="form-control" type="email" value="{{ $data_guru->email }}" name="email_guru">
+                                  </div>
+                              </div>
+                          </div>
+                          {{-- <div class="row">
+                              <div class="col-md-6">
+                                  <div class="form-group">
+                                      <img class="rounded img-thumbnail mb-2" src="{{ asset('profile_img/guru/'.$data_guru->profile_img )}}" alt="foto guru" >
+                                      <input type="file" name="profile_img" class="form-control">
+                                  </div>
+                              </div>
+                          </div> --}}
+                          <button type="submit" class="btn btn-warning pull-right">Update Account</button>
+                          <div class="clearfix"></div>
+                    </form>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/halaman/walikelas/index.blade.php
+++ resources/views/halaman/walikelas/index.blade.php
@@ -21,16 +21,32 @@
     <div class="col-lg-3 col-md-6 col-sm-6">
         <div class="card card-stats">
             <div class="card-header card-header-info card-header-icon">
-                <p class="card-category">Siswa Kelas {{ $nama_kelas }}</p>
-                <h4 class="card-title">{{ $n_siswa_kelas }} <small>Orang</small></h4>
+                <p class="card-category">Nama Kelas</p>
+                <h4 class="card-title"> {{ $nama_kelas }}</h4>
             </div>
         </div>
     </div>
     <div class="col-lg-3 col-md-6 col-sm-6">
         <div class="card card-stats">
             <div class="card-header card-header-info card-header-icon">
-                <p class="card-category">Kelas</p>
-                <h4 class="card-title"> {{ $nama_kelas }}</h4>
+                <p class="card-category">Tahun Ajaran</p>
+                <h4 class="card-title"> {{ $tahun_ajaran }}</h4>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+            <div class="card-header card-header-info card-header-icon">
+                <p class="card-category">Semester</p>
+                <h4 class="card-title"> {{ $semester }}</h4>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+            <div class="card-header card-header-info card-header-icon">
+                <p class="card-category">Siswa Kelas {{ $nama_kelas }}</p>
+                <h4 class="card-title">{{ $n_siswa_kelas }} <small>Orang</small></h4>
             </div>
         </div>
     </div>
